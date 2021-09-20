f2d6f57e4adc20d3281b88442254371bfbeaef70
xiaoheixi <justinzhao893@gmail.com>
2021-09-20 21:50:28 +1000

    AdminController.php creaetd.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function index(){
-        return view('admin.adminHomePage');
+        return view('welcome');
     }
 }
