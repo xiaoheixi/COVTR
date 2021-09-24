9f64af21335661e137a4ff9407ff470ef6e9984a
xiaoheixi <justinzhao893@gmail.com>
2021-09-24 15:54:08 +1000

    Migrated Locked Down Locations.

--- database/migrations/2021_09_24_054650_create_locked_down_locations_table.php
+++ database/migrations/2021_09_24_054650_create_locked_down_locations_table.php
@@ -14,7 +14,9 @@ class CreateLockedDownLocationsTable extends Migration
     public function up()
     {
         Schema::create('locked_down_locations', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('Location');
+            $table->integer('Days');
             $table->timestamps();
         });
     }
