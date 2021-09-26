c8e407e44cd0c670052a60684269b3fb6383377d
xiaoheixi <justinzhao893@gmail.com>
2021-09-26 16:10:39 +1000

    Table structure on adminHomePage.blade.php created.

--- resources/views/admin/adminHomePage.blade.php
+++ resources/views/admin/adminHomePage.blade.php
@@ -4,4 +4,21 @@
 <h1>Dashboard</h1>
 <p>{{ $numberOfLockedDownLocations }}</p>
 <p>{{ $numberOfUsers }}</p>
+<p>Locked Down Locations</p>
+<button>Add Locked Down Location</button>
+<table>
+    <tr>
+        <th>Location</th>
+        <th>Days</th>
+        <th>Modify</th>
+        <th>Delete</th>
+</table>
+<p>Users</p>
+<button>Add User</button>
+<table>
+    <tr>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Account Status</th>
+</table>
 @endsection
