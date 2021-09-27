2e58a586fded97ac8a7d09be61253732f2c16319
xiaoheixi <justinzhao893@gmail.com>
2021-09-27 16:03:49 +1000

    dd($locations)

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Location;
 
 class LocationsController extends Controller
 {
@@ -13,6 +14,8 @@ class LocationsController extends Controller
      */
     public function index()
     {
+        $locations = Location::all();
+        dd($locations);
         return view('locations.index');
     }
 
