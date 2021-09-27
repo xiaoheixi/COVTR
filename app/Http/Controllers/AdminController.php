027b4817f32c78b41e0b730f0f9d678522fe8a6f
xiaoheixi <justinzhao893@gmail.com>
2021-09-27 16:47:35 +1000

    AdminController updated.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,34 +3,33 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App;
 
 class AdminController extends Controller
 {
     public function index(){
-        $numberOfLockedDownLocations = DB::table('locked_down_locations')
+        $numberOfLockedDownLocations = DB::table('locations')
         ->count();
         $numberOfUsers = "Number of Users";
-        $LockedDownLocations = DB::table('locked_down_locations')
-            ->get();
-        dd($numberOfLockedDownLocations);
-        dd($LockedDownLocations);
-        //return view('admin.adminHomePage', compact('numberOfLockedDownLocations', 'numberOfUsers'));
+        $locations = DB::table('locations')
+        ->get();
+        return view('admin.adminHomePage', compact('numberOfLockedDownLocations', 'numberOfUsers', 'locations'));
     }
     public function addLockedDownLocation(){
-        $lockedDownLocation = DB::table('locked_down_locations')
+        $lockedDownLocation = DB::table('locations')
         ->insert([
             'location' => 'New Location', 'days' => 'New Days'
         ]);
     }
     public function modifyLockedDownLocation(){
-        $lockedDownLocation = DB::table('locked_down_locations')
+        $lockedDownLocation = DB::table('locations')
         ->where('id', '=', 15)
         ->update([
             'location' => 'Modified Location', 'days' => 'Modified Days'
         ]);
     }
     public function deleteLockedDownLocation(){
-        $lockedDownLocation = DB::table('locked_down_locations')
+        $lockedDownLocation = DB::table('locations')
         ->where('id', '=', 15)
         ->delete();
     }
