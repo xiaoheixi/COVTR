b16cfd9a8586579adedc73a69c5b33353ce33f84
xiaoheixi <justinzhao893@gmail.com>
2021-09-28 15:23:09 +1000

    Form added in create.blade.php for locations.

---
+++ resources/views/locations/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Add Lockdown Location</h1>
+    <form action="">
+        <input
+            type="text"
+            name="location"
+            placeholder="location">
+    </form>
+@endsection
