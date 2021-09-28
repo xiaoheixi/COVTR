4666cf003efc7c8099f27fd0060be9759c6828e2
xiaoheixi <justinzhao893@gmail.com>
2021-09-28 15:29:58 +1000

    @csrf added.

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <h1>Add Lockdown Location</h1>
-    <form action="">
+    <form action="/locations" method="POST">
+        @csrf
         <input type="text" name="location" placeholder="Location">
         <input type="text" name="days" placeholder="Days">
         <button type="submit">
