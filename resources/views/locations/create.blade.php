d5df8a4283f0ab8851a6a7e70b4511f2ab452c5c
xiaoheixi <justinzhao893@gmail.com>
2021-09-28 15:27:29 +1000

    create.blade.php updated.

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -3,9 +3,10 @@
 @section('content')
     <h1>Add Lockdown Location</h1>
     <form action="">
-        <input
-            type="text"
-            name="location"
-            placeholder="location">
+        <input type="text" name="location" placeholder="Location">
+        <input type="text" name="days" placeholder="Days">
+        <button type="submit">
+            Submit
+        </button>
     </form>
 @endsection
